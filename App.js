1c44c020f552f18e0418e44d9ce35e656793ad35
Youssef Assad <youssefassad158@gmail.com>
2023-12-04 19:30:50 +0100

    first Commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ImageGenerator from './Components/imageGenerator/imageGenrator';
+
+function App() {
+  return (
+   <>
+   <ImageGenerator/>
+   </>
+   
+  );
+}
+
+export default App;
